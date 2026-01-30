d8a0c7649987db188a1fdc14de127785ec41f5bb
zhoujg <22626496@qq.com>
2026-01-30 23:41:12 +0800

    添加缺失的 module 和 scss 文件
    - 创建 package-list.module.ts
    - 创建 exam-list.module.ts
    - 创建 package-list.page.scss
    - 创建 exam-list.page.scss
    - 修复编译错误

---
+++ frontend/src/app/pages/exam-list/exam-list.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { RouterModule, Routes } from '@angular/router';
+
+import { ExamListPage } from './exam-list.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ExamListPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [ExamListPage]
+})
+export class ExamListPageModule {}
---
+++ frontend/src/app/pages/package-list/package-list.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { RouterModule, Routes } from '@angular/router';
+
+import { PackageListPage } from './package-list.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PackageListPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [PackageListPage]
+})
+export class PackageListPageModule {}
